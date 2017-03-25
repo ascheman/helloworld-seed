6cee72bf1d42a85fe37b4f70b4ca0070295f717a
Gerd Aschemann <gerd@aschemann.net>
2017-03-25 20:26:10 +0100

    Extend the build shell script

--- helloworldInstallMavenDocker.groovy
+++ helloworldInstallMavenDocker.groovy
@@ -9,6 +9,12 @@ job ("helloworld-install-mavendocker") {
         scm("H/10 * * * *")
     }
     steps {
-        shell ('docker run -i --rm --name hello-world-install -v ~/.m2:/root/.m2 -v "$PWD":/usr/src -w /usr/src maven:alpine mvn clean install')
+        shell ('''
+volumes="-v "$PWD":/usr/src"
+test -d $HOME/.m2 && volumes="$volumes -v $HOME/.m2:/root/.m2"
+links=""
+test -r $HOME/.docker-nexus-host && links="--link `cat $HOME/.docker-nexus-host`:nexus" 
+docker run -i --rm --name hello-world-install $volumes $links -w /usr/src maven:alpine mvn clean install
+''')
     }
 }
