e461a86e37579c873fc4e382e2265522ab7c2240
Gerd Aschemann <gerd@aschemann.net>
2017-03-25 21:15:52 +0100

    Deploy helloworld artifacts

---
+++ helloworldDeployMavenDocker.groovy
@@ -0,0 +1,20 @@
+/**
+ * Created by ascheman on 25.03.17.
+ */
+job ("helloworld-deploy-mavendocker") {
+    scm {
+        github ("devopssquare/helloworld", "master")
+    }
+    triggers {
+        scm("H/10 * * * *")
+    }
+    steps {
+        shell ('''
+volumes="-v "$PWD":/usr/src"
+test -d $HOME/.m2 && volumes="$volumes -v $HOME/.m2:/root/.m2"
+links=""
+test -r $HOME/.docker-nexus-host && links="--link `cat $HOME/.docker-nexus-host`:nexus" 
+docker run -i --rm --name hello-world-install $volumes $links -w /usr/src maven:alpine mvn clean deploy
+''')
+    }
+}
